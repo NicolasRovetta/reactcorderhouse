82fe3dc4bf8fefb84f055850f35c963d4c5e1e63
NicolasRovetta <nico.rovetta@gmail.com>
2024-11-15 00:07:45 -0300

    modif the cart+conecct a css

--- src/cart.jsx
+++ src/cart.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import cart from './assets/cart.png';
+import './cart.css'
 
 function Cart({ cartItems }) {
   return (
     <div className="cart">
       <a>
-        <img src={cart} alt="Carrito de compras" />
+        <img src={cart} alt="Carrito de compras"/>
       </a>
     </div>
   );
